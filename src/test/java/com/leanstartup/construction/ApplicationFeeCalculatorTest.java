5ec53d91b23ab50df68b337d12ef931d893f6b57
karengreene <greene.karen@gmail.com>
2014-05-05 17:55:52 -0400

    use case1

---
+++ src/test/java/com/leanstartup/construction/ApplicationFeeCalculatorTest.java
@@ -0,0 +1,33 @@
+package com.leanstartup.construction;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApplicationFeeCalculatorTest {
+
+	@Test
+	public void testCalculateFeeAbove100000(){
+		
+		BigDecimal feeAcessed = ApplicationFeeCalculator.calculateFee(new BigDecimal("200000"));
+		
+		Assert.assertEquals(new BigDecimal("30000.00"),feeAcessed);
+	}
+	
+	@Test
+	public void testCalculateFeeBelow100000(){
+		
+		BigDecimal feeAcessed = ApplicationFeeCalculator.calculateFee(new BigDecimal("50000"));
+		
+		Assert.assertEquals(new BigDecimal("10000.00"),feeAcessed);
+	}
+	
+	@Test
+	public void testCalculateAt100000(){
+		
+		BigDecimal feeAcessed = ApplicationFeeCalculator.calculateFee(new BigDecimal("100000"));
+		
+		Assert.assertEquals(new BigDecimal("20000.00"),feeAcessed);
+	}
+}
